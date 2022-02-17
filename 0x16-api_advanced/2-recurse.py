dc0343c3a720ae280fc9ccdc3cc7fcc68995c2ea
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-02-17 08:31:38 -0800

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""
-Contains the recurse function
-"""
+"""contains function that queries the reddit api"""
 
-import requests
+from requests import get
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """returns a list of all hot post titles for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """returns the a list of all hot posts of a subreddit"""
+    params = {"limit": 50}
+    if after:
+        params["after"] = after
+    req = get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={
+            "User-Agent": "alx_app"},
+        params=params,
+        allow_redirects=False)
+    if req.status_code != 200:
         return None
-    r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
-                     headers={'User-Agent': 'Python/requests:APIproject:\
-                     v1.0.0 (by /u/aaorrico23)'},
-                     params={'after': after}).json()
-    after = r.get('data', {}).get('after', None)
-    posts = r.get('data', {}).get('children', None)
-    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
-        if len(hot_list) == 0:
-            return None
-        return hot_list
     else:
+        posts = req.json().get("data").get("children")
         for post in posts:
-            hot_list.append(post.get('data', {}).get('title', None))
-    if after is None:
-        if len(hot_list) == 0:
-            return None
-        return hot_list
-    else:
-        return recurse(subreddit, hot_list, after)
+            hot_list.append(post.get("data").get("title"))
+        if req.json().get("data").get("after"):
+            return recurse(
+                subreddit,
+                hot_list=hot_list,
+                after=req.json().get("data").get("after"))
+        else:
+            return hot_list
